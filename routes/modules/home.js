4e478690eae548a46aa3dd065832e111e82594b2
Derek Hsu <derek800109@gmail.com>
2020-11-14 22:32:42 +0800

    feat: add month filter to index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,17 +6,14 @@ const Record = require('../../models/record')
 
 //----------------------------------------------------------------------------------
 
-function get_total_amount(category, records) {
+function get_total_amount(records) {
   let total_amount = 0
 
-  const amount_array = records
-    .filter(record => category === null || record.category._id == category)
-    .map(record => record.amount)
-
-  if (amount_array.length === 0) {
+  if (records.length === 0) {
     return total_amount
   }
 
+  const amount_array = records.map(record => record.amount)
   total_amount = amount_array.reduce((a, b) => a + b)
 
   return total_amount
@@ -37,39 +34,43 @@ function get_yyyymmdd(date) {
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  const total_amount_category = req.query.filter || null
-  console.log(' ****** ' + total_amount_category)
+  const currentCategory = req.query.category || "類別"
+  const currentMonth = req.query.month || '月份'
+  let months = ['月份'].concat(Array.from({ length: 12 }, (_, i) => (i + 1).toString()))
+  console.log('month:' + currentMonth)
 
   Category
-    .find()
-    .lean()
-    .then(categories => {
-      categories.push({ category: "全部" })
-      const currentCategory = categories.filter(category => category._id == total_amount_category)[0].category || "全部"
+      .find()
+      .lean()
+      .then(categories => {
+        categories.push({ category: "類別" })
 
-      Record.find({ userId }) // 取出 Todo model 裡的所有資料
-        .lean()
-        .populate({ path: 'category', names: ['category', 'icon'] })
-        .then(records => {
-          const totalAmount = get_total_amount(total_amount_category, records)
-          records = records.filter(record => record.category.category === currentCategory || currentCategory === "全部")
-          records = records.map(record => {
-                        return {
-                          _id: record._id,
-                          name: record.name,
-                          category: record.category,
-                          date: get_yyyymmdd(record.date),
-                          amount: record.amount,
-                          merchant: record.merchant
-                        }
-                      })
+        Record.find({ userId }) // 取出 Todo model 裡的所有資料
+          .lean()
+          .populate({ path: 'category', names: ['category', 'icon'] })
+          .then(records => {
+            records = records.filter(record => record.category.category === currentCategory || currentCategory === "類別")
+            records = records.filter(record => (record.date.getMonth() + 1).toString() === currentMonth || currentMonth === '月份')
+            console.log(records.map(record => record.date.getMonth()))
 
+            const totalAmount = get_total_amount(records)
 
-          res.render('index', { records, categories, totalAmount, currentCategory })
-        })
-        .catch(error => console.error(error))
-    })
-    .catch(error => console.error(error))
+            records = records.map(record => {
+              return {
+                _id: record._id,
+                name: record.name,
+                category: record.category,
+                date: get_yyyymmdd(record.date),
+                amount: record.amount,
+                merchant: record.merchant
+              }
+            })
+
+            res.render('index', { records, categories, months, totalAmount, currentCategory, currentMonth })
+          })
+          .catch(error => console.error(error))
+      })
+      .catch(error => console.error(error))
 })
 
 //----------------------------------------------------------------------------------
